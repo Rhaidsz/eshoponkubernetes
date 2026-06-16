a38b861139c6544f0ccc8edc5ea883cd99636d13
Kamil Buruk <kamilb1994@hotmail.de>
2026-06-16 22:06:37 +0000

    Extensions.cs angepasst

--- src/WebApp/Extensions/Extensions.cs
+++ src/WebApp/Extensions/Extensions.cs
@@ -76,8 +76,11 @@ public static class Extensions
 
 	    options.PushedAuthorizationBehavior = PushedAuthorizationBehavior.Disable; // funktioniert
 
-	    options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // Neu wegen Cookieproblem
-	    options.NonceCookie.SecurePolicy = CookieSecurePolicy.SameAsRequest // Neu wegen Cookieproblem
+	    options.CorrelationCookie.SameSite = SameSiteMode.Lax; // Neu wegen Cookieproblem
+	    options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.None; // Neu wegen Cookieproblem
+
+	    options.NonceCookie.SameSite = SameSiteMode.Lax; // Neu wegen Cookieproblem
+	    options.NonceCookie.SecurePolicy = CookieSecurePolicy.None; // Neu wegen Cookieproblem
 
             options.SignedOutRedirectUri = callBackUrl;
             options.ClientId = "webapp";
